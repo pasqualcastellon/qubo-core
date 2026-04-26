d9c278d7961ba8c9031eb7288158279507a76b81
Pasqual Castellon Gadea <p.castellon@cadt.com>
2026-04-26 13:13:06 +0200

    chore: fix typing

--- src/stt_qubo_core/__init__.py
+++ src/stt_qubo_core/__init__.py
@@ -1,12 +1,16 @@
 from .models import QuboProblem
 from .value_objects import QuboDict
 from .result import QuboSolution, QuboSolveResult
+from .ports import QuboSolverPort
 from .services import QuboService
+from .solvers.dimod_sa import DimodSimulatedAnnealingSolver
 
 __all__ = [
     "QuboProblem",
     "QuboDict",
     "QuboSolution",
     "QuboSolveResult",
+    "QuboSolverPort",
     "QuboService",
+    "DimodSimulatedAnnealingSolver"
 ]
--- src/stt_qubo_core/solvers/dimod_sa.py
+++ src/stt_qubo_core/solvers/dimod_sa.py
@@ -1,18 +1,27 @@
 import dimod
+import uuid
+from dimod.typing import Bias, Variable
+from typing import Dict, Tuple, Mapping, cast
+from ..ports import QuboSolverPort
 from ..models import QuboProblem
 from ..result import QuboSolveResult, QuboSolution
 
 
-class DimodSimulatedAnnealingSolver:
+class DimodSimulatedAnnealingSolver(QuboSolverPort):
+    """
+    QUBO solver implemented with dimod's SimulatedAnnealingSampler.
+    """
     def solve(self, problem: QuboProblem) -> QuboSolveResult:
         sampler = dimod.SimulatedAnnealingSampler()
-        sample_set = sampler.sample_qubo(problem.qubo.data, num_reads=problem.num_reads)
+        Q = cast(Mapping[Tuple[Variable, Variable], Bias], problem.qubo.data)
+        sample_set = sampler.sample_qubo(Q, num_reads=problem.num_reads)
 
-        solutions = []
+        solutions: list[QuboSolution] = []
         for row in sample_set.data():
-            sample = {str(k): int(v) for k, v in row.sample.items()}
+            sample: Dict[str, float | int]  = {str(k): int(v) for k, v in row.sample.items()}
             solutions.append(
                 QuboSolution(
+                    id=str(uuid.uuid4()),
                     sample=sample,
                     energy=float(row.energy),
                 )
